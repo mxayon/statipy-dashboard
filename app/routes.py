6987bc56cbae6bfd8a851f40f8fead408ec0f60b
mxayon <mxnkpl@gmail.com>
2019-08-10 11:05:17 -0700

    implements first route to index

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,17 @@
 from flask import Flask
 from app import app
+
+@app.route('/')
+@app.route('/index')
+def home():
+    user = { 'username': 'Max' }
+    return '''
+    <html>
+    <head>
+        <title>Statipy Dashboard</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+    </html>
+    '''

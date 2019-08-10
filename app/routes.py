adbb9d805f1be38f84c3bbe199609c5c1b70fcd9
mxayon <mxnkpl@gmail.com>
2019-08-10 12:04:33 -0700

    adds post data to route for index

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,13 @@ from app import app
 @app.route('/index')
 def home():
     user = { 'username': 'Max' }
-    return render_template('index.html', title='Home | Statipy', user=user)
+    views = [
+    {
+        'title': {'date': 'Artist'},
+        'body': 'Artists in playlist by date'
+    },
+    {
+        'title': {'date': 'Song Rating: PG-13/ALL'},
+        'body': 'Song rating of tracks in playlist by date'
+    }
+    return render_template('index.html', title='Statipy', user=user)

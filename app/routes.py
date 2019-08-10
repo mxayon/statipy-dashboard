13c024198f102cb0cb45769c82ffd5a094c269db
mxayon <mxnkpl@gmail.com>
2019-08-10 11:16:27 -0700

    connects route to render index

--- app/routes.py
+++ app/routes.py
@@ -5,13 +5,4 @@ from app import app
 @app.route('/index')
 def home():
     user = { 'username': 'Max' }
-    return '''
-    <html>
-    <head>
-        <title>Statipy Dashboard</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-    </html>
-    '''
+    return render_template('index.html', title='Home | Statipy', user=user)

9244d8f8455373a07dd5daad39d26dbe46449ede
mxayon <mxnkpl@gmail.com>
2019-08-10 12:43:07 -0700

    injects data to layout and index

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template
+from flask import render_template, url_for
 from app import app
 
 @app.route('/')
 @app.route('/index')
-def home():
+def index():
     user = { 'username': 'Max' }
     views = [
-    {
-        'title': {'date': 'Artist'},
-        'body': 'Artists in playlist by date'
-    },
-    {
-        'title': {'date': 'Song Rating: PG-13/ALL'},
-        'body': 'Song rating of tracks in playlist by date'
-    }
-    return render_template('index.html', title='Statipy', user=user)
+        {
+            'head': 'Artist',
+            'body': 'Artists in playlist by date'
+        },
+        {
+            'head': 'Song Rating: PG-13/ALL',
+            'body': 'Song rating of tracks in playlist by date'
+        }
+    ]
+    return render_template('index.html', title='Statipy', user=user, views=views)
